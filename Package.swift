e2d8e0b90d32f8ba640277cb38019055af0c01f2
Brian Dewey <bdewey@gmail.com>
2021-01-08 06:48:46 -0800

    Get target names, platform, & resources

--- Package.swift
+++ Package.swift
@@ -4,12 +4,13 @@
 import PackageDescription
 
 let package = Package(
-    name: "AztecEditor-iOS",
+    name: "Aztec",
+    platforms: [.iOS(.v11)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "AztecEditor-iOS",
-            targets: ["AztecEditor-iOS"]),
+            name: "Aztec",
+            targets: ["Aztec"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +20,15 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "AztecEditor-iOS",
-            dependencies: []),
+            name: "Aztec",
+            dependencies: [],
+            path: "Aztec/Classes",
+            resources: [.copy("Aztec/Assets/html_colors.json"), .copy("Aztec/Assets/Media.xcassets")]
+        ),
         .testTarget(
             name: "AztecEditor-iOSTests",
-            dependencies: ["AztecEditor-iOS"]),
+            dependencies: ["Aztec"],
+            path: "AztecTests"
+        ),
     ]
 )
